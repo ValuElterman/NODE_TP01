850fc89785dbd95d3d6e9b6c635099b0639ff291
valen <48855001@est.ort.edu.ar>
2026-03-13 09:00:18 -0300

    first commit

---
+++ EJ01.js
@@ -0,0 +1,8 @@
+const string1 = "Aguante";
+const string2 = "Larry";
+
+const concatenar = string1 + " " + string2;
+console.log(concatenar);
+
+const interpolar = `${string1} ${string2}`;
+console.log(interpolar);
